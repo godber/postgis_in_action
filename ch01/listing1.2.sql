780362eb45959290c1df931f6505344d5ddd88fa
Austin Godber <godber@uberhip.com>
2016-06-06 07:48:02 -0700

    l1.2

--- ch01/listing1.2.sql
+++ ch01/listing1.2.sql
@@ -2,3 +2,16 @@ CREATE TABLE ch01.lu_franchises (
     id char(3) PRIMARY KEY,
     franchises varchar(30)
 );
+
+INSERT INTO ch01.lu_franchises(id, franchises)
+VALUES
+    ('BKG', 'Burger King'),
+    ('CJR', 'Carl''s Jr'),
+    ('HDE', 'Hardee'),
+    ('INO', 'In-N-Out'),
+    ('JIB', 'Jack in the Box'),
+    ('KFC', 'Kentucy Fried Chicken'),
+    ('MCD', 'McDonald'),
+    ('PZH', 'Pizza Hut'),
+    ('TCB', 'Taco Bell'),
+    ('WDY', 'Wendy''s')
