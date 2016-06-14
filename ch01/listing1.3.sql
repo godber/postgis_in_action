de821d32178a57d8b5f6cc9fb08fce18dc933740
Austin Godber <godber@uberhip.com>
2016-06-14 06:18:06 -0700

    added loading of restaurants table

--- ch01/listing1.3.sql
+++ ch01/listing1.3.sql
@@ -16,3 +16,22 @@ ALTER TABLE ch01.restaurants
     REFERENCES ch01.lu_franchises (id) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 CREATE INDEX fki_restaurants_franchises ON ch01.restaurants (franchise);
+
+CREATE TABLE ch01.restaurants_staging (
+    franchise text,
+    lat double precision,
+    lon double precision
+);
+
+-- Load restaurant staging table from CSV
+
+-- \copy ch01.restaurants_staging FROM 'book/ch01/data/restaurants.csv' DELIMITER as ',';
+
+
+-- Copy from staging table into restaurants table
+
+-- INSERT INTO ch01.restaurants (franchise, geom)
+-- SELECT
+--     franchise,
+--     ST_Transform(ST_SetSRID(ST_Point(lon, lat), 4326), 2163) AS geom
+-- FROM ch01.restaurants_staging;
